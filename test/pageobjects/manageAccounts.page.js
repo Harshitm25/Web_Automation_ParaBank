b80528d9766a53d74b32a02a04ffde2bc0f182b5
User <user@TestVagrants-MacBook-Pro.local>
2023-09-11 18:48:25 +0530

    final manageAccount.page

---
+++ test/pageobjects/manageAccounts.page.js
@@ -0,0 +1,91 @@
+class ManageAccount{
+
+    get systemBtn()
+    {
+        return $('#coreapps-systemadministration-homepageLink-coreapps-systemadministration-homepageLink-extension')
+    }
+    get manageBtn()
+    {
+        return $('#org-openmrs-module-adminui-accounts-app')
+    }
+    get accntBtnExist()
+    {
+        return $('#content > a')
+    }
+
+    get familyNameBtn()
+    {
+        return $('#adminui-familyName-field')
+    }
+
+    get givenNameBtn()
+    {
+        return $('#adminui-givenName-field')
+    }
+    get nextClick()
+    {
+        return $('#adminui-gender-0-field')
+    }
+
+    get addUserAcntBtn()
+    {
+        return $('#adminui-addUserAccount')
+    }
+
+    get userNameBtn()
+    {
+        return $('#adminui-username-field')
+    }
+
+    get selectBtn()
+    {
+        return $('#adminui-privilegeLevel-field')
+    }
+
+    get newPassBtn()
+    {
+        return $('#adminui-password-field')
+    }
+
+    get confmPassBtn()
+    {
+        return $('#adminui-confirmPassword-field')
+    }
+
+    get capabiltyCheck()
+    {
+        return $('//*[@id="adminui-capabilities-Application: Administers System"]')
+    }
+
+    get addProviderBtn()
+    {
+        return $('#adminui-addProviderAccount')
+    }
+
+    get setValueForAddBtn()
+    {
+        return $('#adminui-identifier-field')
+    }
+
+    get providerRoleBtn()
+    {
+        return $('#adminui-providerRole-field')
+    }
+
+    get saveBtn()
+    {
+        return $('#save-button')
+    }
+    get searchName()
+    {
+        return $('//*[@id="list-accounts_filter"]/label/input')
+    }
+
+    get searchbarResult()
+    {
+        return $('//*[@id="list-accounts"]/tbody/tr/td[1]')
+    }
+
+}
+
+module.exports = new ManageAccount();
