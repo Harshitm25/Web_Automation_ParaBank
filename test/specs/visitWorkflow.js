9a5298e38941e832fefbcf6e2fd3cbd8635eb6f4
User <user@TestVagrants-MacBook-Pro.local>
2023-09-11 18:45:21 +0530

    final visitworkflow

--- test/specs/visitWorkflow.js
+++ test/specs/visitWorkflow.js
@@ -1,19 +1,20 @@
 //const { expect } = require("chai");
-
+const loginPage=require( "../pageobjects/login.page");
+const visitWorkFlow=require('../pageobjects/visitworkflow.page')
 describe('changing password',()=>
 {
     it('should login', async()=>{
         await browser.url('/');
-        await $("#username").setValue("dhoni");
-        await $("#password").setValue("Msdhoni12");
-        await $("#Pharmacy").click();
-        await $("#loginButton").click();
+        await loginPage.inputUsername.setValue("dhoni");
+        await loginPage.inputPassword.setValue("Msdhoni14");    //change
+        await loginPage.pharmacyBtn.click();
+        await loginPage.btnSubmit.click();
         await browser.pause(3000)
     })
 
     it("click on register a patient",async()=>
     {
-        let selector=await $('//*[@id="referenceapplication-registrationapp-registerPatient-homepageLink-referenceapplication-registrationapp-registerPatient-homepageLink-extension"]').click();
+        let selector=await visitWorkFlow.registerPatientBtn.click();
         await browser.keys(['T', 'e', 's', 't']);
         await browser.keys(['\uE007']);
         await browser.keys(['\uE007']);
@@ -21,23 +22,23 @@ describe('changing password',()=>
         await browser.keys(['h', 'a', 'r', 's']);
         await browser.pause(4000)
         //await $('.required focused ui-autocomplete-input error').setValue('harshit');
-        await $('#next-button').click();
-        await $('//*[@id="gender-field"]/option[1]').click();
-        await $('#next-button').click();
-        await $('#birthdateYears-field').setValue('12');
-        await $('#birthdateMonths-field').setValue('10');
-        await $('#next-button').click();
-        await $('#address1').setValue('abc');
-        await $('#next-button').click();
+        await visitWorkFlow.nextBtn.click();
+        await visitWorkFlow.genderBtn.click();
+        await visitWorkFlow.nextBtn.click();
+        await visitWorkFlow.birthYears.setValue('12');
+        await visitWorkFlow.birthMonth.setValue('10');
+        await visitWorkFlow.nextBtn.click();
+        await visitWorkFlow.addressBtn.setValue('abc');
+        await visitWorkFlow.nextBtn.click();
         //await $('//*[@id="fr1132-field"]').setValue('012');
-         await $('#next-button').click();
-        await $('#next-button').click();
+         await visitWorkFlow.nextBtn.click();
+        await visitWorkFlow.nextBtn.click();
        
 
         // await browser.keys(['']);
         // await browser.keys(['Enter']);
 
-        await $('#submit').click();
+        await visitWorkFlow.submitBtn.click();
 
         await browser.pause(5000)
        
@@ -45,24 +46,24 @@ describe('changing password',()=>
 
     it("start visiting workflow and test start visit button ",async()=>
     {
-        let startvisitBtn=await $('//*[@id="org.openmrs.module.coreapps.createVisit"]/div/div[2]');
+        let startvisitBtn=await visitWorkFlow.startVisitingBtn;
         await expect(startvisitBtn).toBeExisting();
 
         await $(startvisitBtn).click();
-        await $('#start-visit-with-visittype-confirm').click();
+        await visitWorkFlow.confirmStartVisitingButton.click();
         await browser.pause(3000)
     })
 
     it("end visit workflow and test end visit button",async()=>
     {
 
-        let endVisitBtn= await $('//*[@id="visit-details"]/div[2]/a[1]');
+        let endVisitBtn= await visitWorkFlow.endVisitBtn;
         await expect(endVisitBtn).toBeExisting();
         await browser.pause(3000)
         await endVisitBtn.click();
         await browser.pause(3000)
 
-         await $('//*[@id="end-visit-dialog"]/div[2]/button[1]').click();
+        await visitWorkFlow.confirmEndVisitBtn.click();
         await browser.pause(3000)
     })
 })
